c9b6a535abc05ae4d8ae375589c2ec7c05cfa7c9
Dawen_ANU <U6828178@anu.edu.au>
2022-09-04 14:30:56 +1000

    fix user register lambda response

--- src/functions/UserRegister/handler.ts
+++ src/functions/UserRegister/handler.ts
@@ -1,25 +1,26 @@
-import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import { PostConfirmationTriggerEvent } from 'aws-lambda';
+import Dynamo from '../../services/DynamoDB';
 
 const UserRegister = async (event: PostConfirmationTriggerEvent) => {
-  console.log('Posted Confirmation triggered: ', event.request.userAttributes);
-  // try {
-  //   const data = {
-  //     UserId: randomUUID(),
-  //     AlbumOwner: event.body.name,
-  //   };
-  //
-  //   await Dynamo.write({
-  //     data: data,
-  //     tableName: process.env.singleTable,
-  //   });
-  //
-  //   console.log('User Register Succeeded');
-  // } catch (e) {
-  //   console.log('User Register Failed: ', e.message);
-  // }
-  return formatJSONResponse({ event });
+  try {
+    const data = {
+      UserId: event.request.userAttributes['sub'],
+      AlbumOwner: event.request.userAttributes['name'],
+      UserEmail: event.request.userAttributes['email'],
+    };
+
+    await Dynamo.write({
+      data: data,
+      tableName: process.env.singleTable,
+    });
+
+    console.log('User Register Succeeded with: ', data);
+  } catch (e) {
+    console.log('User Register Failed: ', e.message);
+  }
+  // return to cognito with {} object as it's a contract requested by cognito.
+  return {};
 };
 
 export const main = middyfy(UserRegister);
