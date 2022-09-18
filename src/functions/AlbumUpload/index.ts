6013e378b0235a413bb5306eef0c955f06d25962
Dawen_ANU <U6828178@anu.edu.au>
2022-09-18 16:08:41 +1000

    fix upload api path

--- src/functions/AlbumUpload/index.ts
+++ src/functions/AlbumUpload/index.ts
@@ -8,7 +8,7 @@ export default {
     {
       http: {
         method: 'post',
-        path: 'retrieve',
+        path: 'upload',
         cors: corsSettings,
         authorizer,
         request: {
